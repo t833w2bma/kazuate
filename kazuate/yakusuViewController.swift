bbd6fce6748aa452ac9ce76d71ce6cc3cc9e500a
t833w2bma <41927549+t833w2bma@users.noreply.github.com>
2018-08-10 07:57:12 +0900

    Update yakusuViewController.swift
    icon

--- kazuate/yakusuViewController.swift
+++ kazuate/yakusuViewController.swift
@@ -10,7 +10,7 @@ class yakusuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textview.text="1~99までで数をあててください"
-        self.ans=Int(arc4random_uniform(100))
+        ans=Int(arc4random_uniform(100))
         
         textview.layer.backgroundColor = UIColor(red: 255/255, green: 248/255, blue: 220/255, alpha: 1.0).cgColor        // BOXデザイン
         textview.layer.borderColor = (UIColor( red: 1, green: 0.95, blue:0.4, alpha: 1.0 )).cgColor
@@ -27,12 +27,12 @@ class yakusuViewController: UIViewController {
         guard let yosou = Int(textfield.text!) else {
             return
         }
-        self.kaisu+=1   //答えた回数をカウント
+        kaisu+=1   //答えた回数をカウント
         //判定して返事を書く
-        if self.ans==yosou {
+        if ans==yosou {
             henji="\nあたり!正解は\(ans)でした。\n  次の問題"
-            self.ans=Int(arc4random_uniform(100))
-        }else if self.ans>yosou{
+            ans=Int(arc4random_uniform(100))
+        }else if ans>yosou{
             henji="\n\(yosou)よりも大きいよ"
           
         }else {
@@ -40,16 +40,19 @@ class yakusuViewController: UIViewController {
         }
         
     //約数ヒント
-        if self.kaisu == 3{  //三回目でヒント
+        
+        if kaisu == 3{  //三回目でヒント
             henji+="､約数は"
-            for k in 2 ..< self.ans/2{
-                if(self.ans%k==0){
-                    henji+=",\(k)"
+            if ans > 4{
+                for k in 2 ..< ans/2{
+                    if(ans%k==0){
+                        henji+=",\(k)"
+                    }
                 }
             }
         }
         
-       self.textview.text=self.textview.text + henji
+       textview.text=textview.text + henji
        textview.scrollRangeToVisible(textview.selectedRange)  //下まで自動スクロール
         textfield.text=""  //回答フィールドを空にする
     }
@@ -63,6 +66,6 @@ class yakusuViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }    
+    }
 
 }
